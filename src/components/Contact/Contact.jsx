bc2cfb2de84501f613029095a90fee48667fdc58
Ashutosh Singh <113412956+ashutosh-singh83@users.noreply.github.com>
2024-10-05 20:20:08 +0530

    Portfolio

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:ashutoshsingh83873@gmail.com">ashutosh@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/ashutosh-singh-2658aa231/">linkedin.com/ashutosh-singh</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/ashutosh-singh83/ashutosh-singh83">github.com/ashutosh-singh</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
